5d5484206190847145fc406a87c499b462642f77
Chris <chris.anderson@teletalkhub.com>
2024-10-17 17:13:12 +0300

    add basics

--- main.py
+++ main.py
@@ -1,30 +1,43 @@
 import fitz
+import tkinter
+from constants import *
 
-def replace_text_in_pdf(input_pdf, output_pdf):
-    doc = fitz.open(input_pdf)
+window = tkinter.Tk()
 
-    for page_num in range(len(doc)):
-        page = doc[page_num]
+window.title('PDF Text Replacer')
+window.iconbitmap('favico.ico')
+window.resizable(False,False)
 
-        print(page.get_fonts())
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+position_top = int(screen_height / 2 - WINDOW_SIZE / 2)
+position_right = int(screen_width / 2 - WINDOW_SIZE / 2)
+window.geometry(f'{WINDOW_SIZE}x{WINDOW_SIZE}+{position_right}+{position_top}')
+
+
+window.mainloop()
+
+# def replace_text_in_pdf(input_pdf, output_pdf):
+#     doc = fitz.open(input_pdf)
+
+#     for page_num in range(len(doc)):
+#         page = doc[page_num]
         
-        text_instances = page.search_for('REPLACE_NAME')
-        for inst in text_instances:
-            page.add_redact_annot(inst, text=input('Enter your name: '), fill=(1, 1, 1))
-            page.apply_redactions()
+#         text_instances = page.search_for('REPLACE_NAME')
+#         for inst in text_instances:
+#             page.add_redact_annot(inst, text=input('Enter your name: '), fill=(1, 1, 1))
+#             page.apply_redactions()
 
-        text_instances = page.search_for('REPLACE_AGE')
-        for inst in text_instances:
-            page.add_redact_annot(inst, text=input('Enter your age: '), fill=(1,1,1))
-            page.apply_redactions()
+#         text_instances = page.search_for('REPLACE_AGE')
+#         for inst in text_instances:
+#             page.add_redact_annot(inst, text=input('Enter your age: '), fill=(1,1,1))
+#             page.apply_redactions()
 
-        
-    
-    doc.save(output_pdf)
-    doc.close()
+#     doc.save(output_pdf)
+#     doc.close()
 
 
-input_pdf = "replace.pdf"
-output_pdf = "modified.pdf"
+# input_pdf = "replace.pdf"
+# output_pdf = "modified.pdf"
 
-replace_text_in_pdf(input_pdf, output_pdf)
+# replace_text_in_pdf(input_pdf, output_pdf)
